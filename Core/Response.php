bee95a0cc067c99819f266fc4b1565c8c3d12745
Ali Hegazy <alihegazy19@outlook.com>
2026-03-13 20:00:11 +0200

    Style: format Response class

--- Core/Response.php
+++ Core/Response.php
@@ -1,17 +1,20 @@
-<?php 
+<?php
 
 namespace Core;
 
-class Response{
+class Response
+{
     private const NOT_FOUND = 404;
     private const FORBIDDEN = 403;
 
-    public static function notFound(){
+    public static function notFound()
+    {
         http_response_code(static::NOT_FOUND);
         view("404");
     }
 
-    public static function forbidden(){
+    public static function forbidden()
+    {
         http_response_code(static::FORBIDDEN);
         view("403");
     }

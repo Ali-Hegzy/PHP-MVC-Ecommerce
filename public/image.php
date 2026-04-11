06d0176197e3bfca07986c4c4b57a60ec42b8f80
Ali Hegazy <alihegazy19@outlook.com>
2026-04-11 12:55:39 +0200

    Enhance image.php for security aspects

--- public/image.php
+++ public/image.php
@@ -2,15 +2,22 @@
 
 const BASE_PATH = __DIR__ . "/../";
 
-$img = $_GET["img"];
+$imageName = $_GET["img"];
 $targetDir = BASE_PATH . "uploads/";
-$targetPath = $targetDir . $img;
+$targetPath = $targetDir . $imageName;
 
-if (file_exists($targetPath)) {
+if (is_dir($targetPath)) {
+    http_response_code(404);
+    echo "Page Not Found";
+    die();
+}
+
+if (file_exists($targetPath) && strpos(realpath($targetPath), realpath($targetDir)) === 0) {
     $mimeType = mime_content_type($targetPath);
 
     header("Content-Type: $mimeType");
     readfile($targetPath);
 } else {
     http_response_code(404);
+    echo "Page Not Found";
 }
